c94ec079c584fac2c5f11f7ce587fb4a7c55acac
KvTsira <kvtsira@gmail.com>
2022-05-02 17:11:10 -0400

    testing for heroku

--- public/service-worker.js
+++ public/service-worker.js
@@ -72,8 +72,6 @@ self.addEventListener("fetch", (evt) => {
     return;
   }
 
-  // if the request is not for the API, serve static assets using "offline-first" approach.
-  // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
   evt.respondWith(
     caches.match(evt.request).then((response) => {
       return response || fetch(evt.request);
